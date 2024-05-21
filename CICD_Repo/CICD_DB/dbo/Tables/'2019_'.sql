f599271469bcfa5c55db8f96edaba8bffc9c47da
vinodkumar <vkbankad@onec1.com>
2024-05-21 08:10:26 +0530

    first commit

---
+++ CICD_Repo/CICD_DB/dbo/Tables/'2019_'.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].['2019$'] (
+    [Country]                          NVARCHAR (255) NULL,
+    [Year]                             DATETIME       NULL,
+    [Rank]                             NVARCHAR (255) NULL,
+    [Total]                            FLOAT (53)     NULL,
+    [C1: Security Apparatus]           FLOAT (53)     NULL,
+    [C2: Factionalized Elites]         FLOAT (53)     NULL,
+    [C3: Group Grievance]              FLOAT (53)     NULL,
+    [E1: Economy]                      FLOAT (53)     NULL,
+    [E2: Economic Inequality]          FLOAT (53)     NULL,
+    [E3: Human Flight and Brain Drain] FLOAT (53)     NULL,
+    [P1: State Legitimacy]             FLOAT (53)     NULL,
+    [P2: Public Services]              FLOAT (53)     NULL,
+    [P3: Human Rights]                 FLOAT (53)     NULL,
+    [S1: Demographic Pressures]        FLOAT (53)     NULL,
+    [S2: Refugees and IDPs]            FLOAT (53)     NULL,
+    [X1: External Intervention]        FLOAT (53)     NULL,
+    [Change from Previous Year]        FLOAT (53)     NULL,
+    [F18]                              NVARCHAR (255) NULL,
+    [F19]                              NVARCHAR (255) NULL
+);
+
